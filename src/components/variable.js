e0d0af02d27a62b7b3dd93c6413fec72900d7834
Marcello Paiva <marcello.paiva@gmail.com>
2020-08-22 17:24:08 -0400

    added prefers reduced motion helper text

--- src/components/variable.js
+++ src/components/variable.js
@@ -14,12 +14,16 @@ var Label = styled.label``
 
 var Option = styled.option``
 
-var Root = styled.div`
+var VariableContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0.5rem 0;
+`
+
+var HelperText = styled.p`
+  font-size: 0.75rem;
 `
 
 export default function Variable({
@@ -38,7 +42,7 @@ export default function Variable({
 
   if (input) {
     return (
-      <Root>
+      <VariableContainer>
         <Label for={name}>{label}:</Label>
         <Input
           name={name}
@@ -50,26 +54,43 @@ export default function Variable({
           min={name === 'thickness' ? '1' : 'any'}
           max={name === 'thickness' ? '10' : 'any'}
         />
-      </Root>
+      </VariableContainer>
     )
   }
 
   return (
-    <Root>
-      <Label for={name}>{label}:</Label>
-      <DropDown
-        name={name}
-        id={name}
-        key={key}
-        onChange={handleChange}
-        value={variableContext.state[name]}
-      >
-        {options.map(option => (
-          <Option value={option.value} key={option.label}>
-            {option.label}
-          </Option>
-        ))}
-      </DropDown>
-    </Root>
+    <>
+      <VariableContainer>
+        <Label for={name}>{label}:</Label>
+        <DropDown
+          name={name}
+          id={name}
+          key={key}
+          onChange={handleChange}
+          value={variableContext.state[name]}
+          aria-describedBy={name === 'motion' ? 'reducedMotion' : ''}
+        >
+          {options.map(option => (
+            <Option value={option.value} key={option.label}>
+              {option.label}
+            </Option>
+          ))}
+        </DropDown>
+      </VariableContainer>
+      {name === 'motion' && (
+        <HelperText id="reducedMotion">
+          If you have Prefers Reduced Motion enabled on your OS, all animations
+          will be disabled. To learn more about Prefers Reduced Motion, check
+          out{' '}
+          <a
+            href="https://css-tricks.com/introduction-reduced-motion-media-query/"
+            aria-lable="An article titled, 'An Introduction to the Reduced Motion Media Query' by Eric Bailey on css-tricks.com"
+          >
+            this article
+          </a>{' '}
+          by Eric Bailey
+        </HelperText>
+      )}
+    </>
   )
 }
