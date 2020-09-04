98b39e15197d6aaabd360e1d7ec600f4af1895b9
Marcello Paiva <marcello.paiva@gmail.com>
2020-09-04 14:44:23 -0400

    worked on variables test

--- cypress/integration/variables/variables.spec.js
+++ cypress/integration/variables/variables.spec.js
@@ -1,7 +1,19 @@
 describe('Focus Style Variables Test', () => {
-  it('loads the variables', () => {
+  it('can edit the variables', () => {
     cy.visit('/')
 
-    cy.contains('Width')
+    cy.get('input[name="width"]')
+      .type('{uparrow}{uparrow}{uparrow}{uparrow}')
+      .should('have.value', '6')
+
+    cy.get('input[name="offset"]')
+      .type('{uparrow}{uparrow}{uparrow}{uparrow}')
+      .should('have.value', '8')
+
+    cy.get('input[name="focusColor"]')
+      .trigger('mousedown')
+      .trigger('mouseup')
+      .tab()
+      .tab()
   })
 })
