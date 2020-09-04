fd7988e66130bcdbdb4ee8764862654d8799158b
Marcello Paiva <marcello.paiva@gmail.com>
2020-09-04 16:13:34 -0400

    added more to variables test

--- cypress/integration/variables/variables.spec.js
+++ cypress/integration/variables/variables.spec.js
@@ -2,6 +2,8 @@ describe('Focus Style Variables Test', () => {
   it('can edit the variables', () => {
     cy.visit('/')
 
+    cy.get('input[name="reducedMotion"]').uncheck()
+
     cy.get('input[name="width"]')
       .type('{uparrow}{uparrow}{uparrow}{uparrow}')
       .should('have.value', '6')
@@ -11,9 +13,35 @@ describe('Focus Style Variables Test', () => {
       .should('have.value', '8')
 
     cy.get('input[name="focusColor"]')
-      .trigger('mousedown')
-      .trigger('mouseup')
-      .tab()
-      .tab()
+      .invoke('val', '#ff0000')
+      .trigger('change')
+      .should('have.value', '#ff0000')
+
+    cy.get('input[name="bgColor"]')
+      .invoke('val', '#000000')
+      .trigger('change')
+      .should('have.value', '#000000')
+
+    cy.get('select[name="outline"]')
+      .invoke('val', 'dotted')
+      .trigger('change')
+      .should('have.value', 'dotted')
+
+    cy.get('select[name="motion"]')
+      .invoke('val', 'bounce')
+      .trigger('change')
+      .should('have.value', 'bounce')
+
+    cy.get('select[name="duration"]')
+      .invoke('val', '2s')
+      .trigger('change')
+      .should('have.value', '2s')
+
+    cy.get('select[name="loop"]')
+      .invoke('val', '2')
+      .trigger('change')
+      .should('have.value', '2')
+
+    cy.contains('Apply').click()
   })
 })
